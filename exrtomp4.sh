a26539976f4746bc9485a5b9931fd029d08132fd
PMoney4k <pascalwiemers@gmail.com>
2024-02-18 01:48:29 +0100

    exrtomp4 now uses the first frame to name the mp4

--- exrtomp4.sh
+++ exrtomp4.sh
@@ -58,14 +58,17 @@ ls *.exr | parallel -v 'oiiotool --ch "R,G,B" --colorconvert "ACES - ACEScg" "Ou
 # Generate a list of .jpg files with the 'file' keyword before each filename
 ls *_converted.jpg | sort -V | sed 's/^/file /' > files.txt
 
+# Use the base name of the first EXR file for the MP4 file name, removing the file extension
+output_filename=$(basename "$first_exr" .exr).mp4
+
 # Stitch .jpg files into a video
-ffmpeg -f concat -safe 0 -i files.txt -c:v libx264 -pix_fmt yuv420p -r $fps -s $res output.mp4
+ffmpeg -f concat -safe 0 -i files.txt -c:v libx264 -pix_fmt yuv420p -r $fps -s $res "$output_filename"
 
 # Navigate back to the original directory
 popd
 
 # Move the output video to the original directory
-mv "$tmpdir/output.mp4" .
+mv "$tmpdir/$output_filename" .
 
 # Remove the temporary directory
 rm -r "$tmpdir"
