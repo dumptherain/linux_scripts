0f1d8eaa9d99df0e6f37f64d1d937b4683228da1
PMoney4k <pascalwiemers@gmail.com>
2024-03-07 12:55:03 +0100

    added the option for mp4.sh to have filenames as input. If no filename is provided it still processes all video in the current directory

--- mp4.sh
+++ mp4.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-mkdir mp4
-for i in *.*; do ffmpeg -i "$i" \
--c:v libx264 -pix_fmt yuv420p \
-./mp4/"${i%.*}.mp4"; done
+mkdir -p mp4
+
+# Check if any arguments are provided
+if [ "$#" -gt 0 ]; then
+    # Process each file provided as an argument
+    for i in "$@"; do
+        ffmpeg -i "$i" \
+        -c:v libx264 -pix_fmt yuv420p \
+        "./mp4/${i%.*}.mp4"
+    done
+else
+    # Process all files in the current directory
+    for i in *.*; do
+        ffmpeg -i "$i" \
+        -c:v libx264 -pix_fmt yuv420p \
+        "./mp4/${i%.*}.mp4"
+    done
+fi
 
 exit
