78594336d344dd7db077ac436af89ba63851f5bc
PMoney4k <pascalwiemers@gmail.com>
2024-05-24 16:32:24 +0200

    made mp4.sh work on video resolution not devisible by 2

--- mp4.sh
+++ mp4.sh
@@ -7,6 +7,7 @@ if [ "$#" -gt 0 ]; then
     # Process each file provided as an argument
     for i in "$@"; do
         ffmpeg -i "$i" \
+        -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" \
         -c:v libx264 -pix_fmt yuv420p \
         "./mp4/${i%.*}.mp4"
     done
@@ -14,6 +15,7 @@ else
     # Process all files in the current directory
     for i in *.*; do
         ffmpeg -i "$i" \
+        -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" \
         -c:v libx264 -pix_fmt yuv420p \
         "./mp4/${i%.*}.mp4"
     done
--- set_wacom.sh
+++ set_wacom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xsetwacom set "Wacom Intuos Pro M Pen stylus" MapToOutput "3840x2160+0+1440"
-xsetwacom set "Wacom Intuos Pro M Pen eraser" MapToOutput "3840x2160+0+1440"
+xsetwacom set "Wacom Intuos Pro M Pen stylus" MapToOutput "3840x2160+1080+1440"
+xsetwacom set "Wacom Intuos Pro M Pen eraser" MapToOutput "3840x2160+1080+1440"
